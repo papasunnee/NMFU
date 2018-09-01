cf6a04d34f3eb58b0317b114a4f389eae59acc7f
Adetona Sunday <papasunnee@gmail.com>
2018-09-01 16:27:43 +0100

    welcomeUpdate

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -72,22 +72,23 @@
                         <a href="{{ url('/home') }}">Home</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
+                        {{-- <a href="{{ route('register') }}">Register</a> --}}
                     @endauth
                 </div>
             @endif
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    N.M.F.U.
+                </div>
+                <div class="m-b-md">
+                    New Members' Fellowship Unit
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="#">About NMFU</a>
+                    <a href="#">News</a>
+                    <a href="#">Contact</a>
                 </div>
             </div>
         </div>
