848bc5adce457de5f4019ab551fe427db26a8c02
Adetona Sunday <papasunnee@gmail.com>
2018-09-01 16:11:34 +0100

    nulldb

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->string('type')->default('user');
             $table->mediumText('bio')->nullable();
-            $table->string('photo')->default('avatar.png');
+            $table->string('photo')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
